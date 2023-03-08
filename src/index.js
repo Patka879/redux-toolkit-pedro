9dc9ffe915080d86e82fbfe0ea5a19acf76b6172
Patka879 <mysiak.patrycja@gmail.com>
2023-03-08 18:45:08 +0100

    $ userReducer imported to index.js and added to the store

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+import userReducer from './features/userSlice'
 
 const store = configureStore({
-  reducer: {}
+  reducer: {
+    user: userReducer,
+  }
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
